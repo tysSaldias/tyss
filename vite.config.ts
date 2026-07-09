2ea187aabf05a8b9dfe47432c62f1213b6a55d23
Tyto <tyto@tyss.cl>
2026-07-09 18:47:14 -0400

    feat: scaffold SvelteKit 5 + TS project with adapter-netlify and Tailwind v4

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import netlifyAdapter from '@sveltejs/adapter-netlify';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			compilerOptions: {
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+			},
+			adapter: netlifyAdapter({ edge: false })
+		})
+	]
+});
